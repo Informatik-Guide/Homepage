29f241b3992283ddb3d3d79adf32b6569cc06f24
MonkeyManuel <dietmar-gartlinger@gmx.de>
2023-11-23 14:26:10 +0100

    Bibi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { TestTutorialComponent } from './pages/test-tutorial/test-tutorial.component';
-import { TestTutorial2Component } from './pages/test-tutorial2/test-tutorial2.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UeberUnsComponent } from './pages/ueber-uns/ueber-uns.component';
 import { GraphenComponent } from './pages/graphen/graphen.component';
 import { ImpressumComponent } from './impressum/impressum.component';
+import { ManuelszoneComponent } from './pages/manuelszone/manuelszone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     NavigationComponent,
-    TestTutorialComponent,
-    TestTutorial2Component,
     PageNotFoundComponent,
     UeberUnsComponent,
     GraphenComponent,
-    ImpressumComponent
+    ImpressumComponent,
+    ManuelszoneComponent
   ],
   imports: [
     BrowserModule,
