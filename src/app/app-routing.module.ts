0fb0e0e83e060348b907d5a8cb009d89207a160e
MonkeyManuel <dietmar-gartlinger@gmx.de>
2024-02-28 21:28:49 +0100

    biggest changes of all time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
+import { LandingComponent } from './landing/landing.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { UeberUnsComponent } from './pages/ueber-uns/ueber-uns.component';
 import { ManuelszoneComponent } from './pages/manuelszone/manuelszone.component';
-import { GraphenComponent } from './pages/graphen/graphen.component';
+import { CodierungComponent } from './Tutorials/codierung/codierung.component';
+import { IfBedingungComponent } from './Tutorials/if-bedingung/if-bedingung.component';
+import { KontaktComponent } from './Kontakt/kontakt.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { CodierungtestComponent } from './Tests/codierungtest/codierungtest.component';
+import { KurzgesagtComponent } from './kurzgesagt/kurzgesagt.component';
+import { IfBedingungtestComponent } from './Tests/if-bedingungtest/if-bedingungtest.component';
 
 const routes: Routes = [
   {path: 'start', component: LandingComponent},
   {path: '', redirectTo: '/start', pathMatch: 'full'},
-  {path: 'ueber_uns', component: UeberUnsComponent},
+  // Main Parts
+  {path: 'kontakt', component: KontaktComponent},
+  {path: 'feedback', component: FeedbackComponent},
+  {path: 'kurzgesagt', component: KurzgesagtComponent},
+  // tmp
   {path: 'manuelszone', component: ManuelszoneComponent},
-  {path: 'graphen', component: GraphenComponent},
+  // Tutorials
+  {path: 'if-bedingung', component: IfBedingungComponent},
+  {path: 'codierung', component: CodierungComponent},
+  // Tests
+  {path: 'codierungtest', component: CodierungtestComponent},
+  {path: 'ifbedingungtest', component: IfBedingungtestComponent},
+  // NotFound
   {path: '**', component: PageNotFoundComponent}
 ];
 
--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./impressum.component.css']
 })
 export class ImpressumComponent {
-
+  
 }
